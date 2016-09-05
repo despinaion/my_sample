ec8df48f2ea5ae8def0eb53bf2969c5f9280c2a2
despinaion <unidepth@hotmail.com>
2016-09-05 21:56:50 +0300

    5.9 Validation & Pagination

--- Gemfile
+++ Gemfile
@@ -56,4 +56,6 @@ gem 'devise'
 
 gem 'cancancan', '~> 1.10'
 
+gem 'will_paginate', '~> 3.1.0'
+
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,9 @@ class Comment < ActiveRecord::Base
 
   scope :rating_asc,  -> { order(rating: :asc) }
 
+  validates :body, presence: true #validation for comment of having text
+  validates :user, presence: true #validation for comment of having user_id
+  validates :product, presence: true #validation for comment of having product_id
+  validates :rating, numericality: { only_integer: true } #validation for comment of having integer rating
+
 end
