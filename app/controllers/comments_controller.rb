bf199e830e0d4cc4b7feb9101bdbddba1e739cb5
despinaion <unidepth@hotmail.com>
2016-09-10 11:47:33 +0300

    5.10 Admin role &Moderating User Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,8 +17,12 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
+  end
 
-	end
 
 	private
 
