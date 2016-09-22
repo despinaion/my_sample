2bd8e0f36fc026652c3caec0a3b403f83e287869
despinaion <unidepth@hotmail.com>
2016-09-22 22:20:08 +0300

    6.10 Ajax in comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,20 +1,22 @@
-$(document).on('ready page:load', function () {
-	$('.img-zoom').elevateZoom({
-        zoomType: "lens",
-        lensShape: "round",
-        lensSize: 180,
-        scrollZoom: true,
-        lensFadeIn: 800,
-        lensFadeOut: 800
-	});
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+var refreshRating = function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
       }
     });
+
+};
+
+$(document).on('ready page:load', function () {
+  refreshRating();
+	$('.img-zoom').elevateZoom({
+    zoomType: "lens",
+    lensShape: "round",
+    lensSize: 180,
+    scrollZoom: true,
+    lensFadeIn: 800,
+    lensFadeOut: 800
+	});
 });
-
-
-
