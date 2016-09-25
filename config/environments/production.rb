57f306ad6aacf174d5101a3c38c500df486db80d
despinaion <unidepth@hotmail.com>
2016-09-25 14:57:32 +0300

    6.11 Caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,13 @@ Rails.application.configure do
 
   # Configure cache store dalli gem, memcached
   config.cache_store = :dalli_store,
-  ENV["MEMCACHIER_SERVERS"] || "").split(","),{
-  :username => ENV["MEMCACHIER_USERNAME"],
-  :password => ENV["MEMCACHIER_PASSWORD"],
-  :failover => true,
-  :socket_timeout => 1.5,
-  :socket_failure_delay => 0.2,
-  :down_retry_delay => 60
-  }
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
+
 end
